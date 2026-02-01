a0dd71cebd6c270ad0abd997721702d4672cca3f
Eyuel-G <eyuelgetachew218@gmail.com>
2026-02-01 04:33:17 +0300

    CORS fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000
 
 const allowedOrigins = [
   "http://localhost:5174",
-  "https://notes-frontend.onrender.com"
+  "https://notes-frontend.onrender.com",
+  "https://notes-frontend-tvd1.onrender.com"
 ];
 
 
